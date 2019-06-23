dd22cc8333d88b36b0688dc78fb955b69681231e
davemaharshi7 <39943075+davemaharshi7@users.noreply.github.com>
2019-06-23 15:09:54 +0530

    Registration part complete
    Event Registration is complete with all details like no seats available and already registered participants

--- app/src/main/java/com/dscddu/dscddu/Fragments/HomeFragment.java
+++ app/src/main/java/com/dscddu/dscddu/Fragments/HomeFragment.java
@@ -1,28 +1,21 @@
 package com.dscddu.dscddu.Fragments;
 
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.dscddu.dscddu.Activities.EventDetailsActivity;
 import com.dscddu.dscddu.Adapters.EventAdapter;
+import com.dscddu.dscddu.Listeners.FragmentActionListener;
 import com.dscddu.dscddu.Model_Class.EventModel;
 import com.dscddu.dscddu.R;
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -33,13 +26,21 @@ public class HomeFragment extends Fragment {
     private FirebaseFirestore rootRef;
     private View rootView;
     private RecyclerView recyclerView;
+    private FragmentActionListener fragmentActionListener;
     private EventAdapter adapter;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Events");
+    }
+    public void setFragmentActionListener(FragmentActionListener fragmentActionListener){
+        this.fragmentActionListener = fragmentActionListener;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -63,16 +64,17 @@ public class HomeFragment extends Fragment {
                 .build();
         adapter = new EventAdapter(con,options);
         recyclerView.setAdapter(adapter);
-        adapter.setOnItemClickListener(new EventAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
-                EventModel e = documentSnapshot.toObject(EventModel.class);
-                String eName = e.getEventName();
-                String docId = documentSnapshot.getId();
-                Intent separate = new Intent(getContext(), EventDetailsActivity.class);
-                separate.putExtra("docId",docId);
-                separate.putExtra("docName",eName);
-                startActivity(separate);
+        adapter.setOnItemClickListener((documentSnapshot, position) -> {
+            EventModel e = documentSnapshot.toObject(EventModel.class);
+            String eName = e.getEventName();
+            String docId = documentSnapshot.getId();
+            if(fragmentActionListener!=null){
+                Bundle bundle = new Bundle();
+                bundle.putInt(FragmentActionListener.ACTION_KEY,
+                        FragmentActionListener.ACTION_VALUE_EVENT_DETAILS);
+                bundle.putString("docId",docId);
+                bundle.putString("eName",eName);
+                fragmentActionListener.actionPerformed(bundle);
             }
         });
         adapter.startListening();
